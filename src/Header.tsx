cd357de3427173f10b55567e67da9c56355adda3
Garett Cooper <garettcooper@ymail.com>
2019-11-13 17:16:02 -0500

    Updated Header to use react-headroom

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,20 +1,23 @@
 import React from 'react';
+import Headroom from 'react-headroom';
 import gitLogo from './Images/ThirdParty/GitHub-Mark.svg';
 import linkedInLogo from './Images/ThirdParty/LI-In-Bug.png';
 
 export default class Header extends React.Component<{}, {}> {
     render() {
         return (
-        <div className="Header">
-            <header>
-                <div className="margin">
-                    <div id="header-bar"/>
-                    Garett Cooper
-                    <a href="https://github.com/GarettCooper"><img src={gitLogo} className="logo" alt="GitHub logo"/></a>
-                    <a href="https://www.linkedin.com/in/garett-cooper-b7b4a8128/"><img src={linkedInLogo} className="logo" alt="LinkedIn logo" /></a>
-                </div>
-            </header>
-        </div>
+        <Headroom>
+<           div className="Header">
+                <header>
+                    <div className="margin">
+                        <div id="header-bar"/>
+                        Garett Cooper
+                        <a href="https://github.com/GarettCooper"><img src={gitLogo} className="logo" alt="GitHub logo"/></a>
+                        <a href="https://www.linkedin.com/in/garett-cooper-b7b4a8128/"><img src={linkedInLogo} className="logo" alt="LinkedIn logo" /></a>
+                    </div>
+                </header>
+            </div>
+        </Headroom>
         );
     }
 }
