3da3757cb06bc49df9917626660c853d02b6e9e2
Garett Cooper <garettcooper@ymail.com>
2019-11-14 12:39:09 -0500

    Setup future pages

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,38 @@ import React from 'react';
 import Header from './Header';
 import GitPanel from './Git'
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <Header/>
-      <div className="margin">
-        <GitPanel/>
-      </div>
-    </div>
-  );
+enum Page {
+  Home,
+  OpenSourceProjects
 }
 
-export default App;
+interface AppState {
+  page: Page
+}
+
+export default class App extends  React.Component<{}, AppState>{
+  constructor (props: {}){
+    super(props)
+    this.state = {
+      page: Page.OpenSourceProjects
+    }
+  }
+
+  render () {
+    let page;
+    switch (this.state.page){
+      case Page.Home:
+      case Page.OpenSourceProjects:
+      default:
+        page = (<GitPanel/>);
+    }
+
+    return (<div className="App">
+      <Header/>
+      <div className="margin">
+        {page}
+      </div>
+    </div>
+    )  
+  }
+}
