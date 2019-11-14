45305b869887621f158fc1a0febb1b6f3d3c79b3
Garett Cooper <garettcooper@ymail.com>
2019-11-14 12:13:06 -0500

    Updated Header css and created CSS follder

--- src/Header.tsx
+++ src/Header.tsx
@@ -7,13 +7,13 @@ export default class Header extends React.Component<{}, {}> {
     render() {
         return (
         <Headroom>
-<           div className="Header">
+<           div className="header drop">
                 <header>
                     <div className="margin">
                         <div id="header-bar"/>
                         Garett Cooper
-                        <a href="https://github.com/GarettCooper"><img src={gitLogo} className="logo" alt="GitHub logo"/></a>
-                        <a href="https://www.linkedin.com/in/garett-cooper-b7b4a8128/"><img src={linkedInLogo} className="logo" alt="LinkedIn logo" /></a>
+                        <a href="https://github.com/GarettCooper"><img src={gitLogo} className="media-widget" alt="GitHub logo"/></a>
+                        <a href="https://www.linkedin.com/in/garett-cooper-b7b4a8128/"><img src={linkedInLogo} className="media-widget" alt="LinkedIn logo" /></a>
                     </div>
                 </header>
             </div>
