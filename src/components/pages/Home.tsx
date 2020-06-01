3e282c2875e76ab9b6ed740099ee15e97cb67db4
Garett Cooper <garettcooper@ymail.com>
2020-05-31 22:45:51 -0400

    Updated the home page to with new information about the gc_nes_emulator

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import {Page, PageProps} from '../App'
+import {isMobile} from 'react-device-detect'
 import '../../css/Home.css'
 
 export default class GitPage extends React.Component<PageProps, {}>{
@@ -17,8 +18,8 @@ export default class GitPage extends React.Component<PageProps, {}>{
                     <p>
                         {window.location.href.indexOf("dev.garettcooper") !== -1 ? (<b>Welcome to the development version of personal website. </b>) : "Welcome to my personal website. "}
                         My name is Garett Cooper, but you probably figured that out already.
-                        For the time being, it is essentially an online résumé with a demonstration of basic React competency, but I plan to continue adding features to it as I have the time.
-                        In future I would like to include a web assembly based version of my <a href="https://github.com/GarettCooper/gc_nes_emulator">gc_nes_emulator</a>, but that will have to wait.
+                        This website is essentially an online résumé with a demonstration of basic React competency, as well as a place to host my WebAssembly based <a href="https://github.com/GarettCooper/gc_nes_emulator">gc_nes_emulator</a>
+                        {isMobile ? ". Switch to a desktop or laptop" : " which allows you"} to play classic Nintendo Entertainment System games in your browser.
                     </p>
                 </div>
                 <div className="home-resume">              
